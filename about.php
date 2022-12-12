139fecf4d2faf078b9b2792416b36bdfd22e81da
mayank7351 <118728993+mayank7351@users.noreply.github.com>
2022-12-13 00:22:39 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>::Leave Management::</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php
+echo "<div class = 'heading'>";
+echo "<h1>Leave Management System</h1>";
+include 'navi.php';
+echo "<center>";
+echo "<h4> In this project, the members are creating a leave management system. The project has the following components:
+•	CLIENT CONSOLE  
+•	ADMIN MANAGEMENT CONSOLE
+•	CLIENT REGISTRATION COMPONENT
+Before accessing the CLIENT CONSOLE, the client has to register himself/herself using the CLIENT REGISTRATION COMPONENT. The registration component consists of a simple HTML form and the confirmation page for the same. Also, the registration details of the same are mailed to the client’s registered email address. The mail feature is present in all the three components and is used as required.
+The CLIENT CONSOLE consists of the following features:
+•	Requesting for a leave Password recovery option (through email)
+•	Seeing the number of leaves available to him/her and all the details about
+•	him/her Viewing the complete profile of him/her with his/her profile picture along with
+•	the all the types of leaves (Casual, Sick and Earn) sanctioned to him/her by the admin
+
+
+ </h4>";
+echo "</center>";
+echo "</div>";
+?>
---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<title>::Leave Management System::</title>
+<link rel="stylesheet" href="style.css">
+<style>
+footer {
+    width:100%;
+    background-color: #808080;
+	position: fixed;
+    bottom: 0;
+	font-family: "Segoe UI Light","Segoe WPC","Segoe UI",
+              Helvetica, Arial, "Arial Unicode MS", Sans-Serif;
+	font-size: 17px;
+	font-style: normal;
+	font-variant: normal;
+	font-weight: 500;
+	height : 30px;
+	text-align: center;
+	box-shadow: 0px 1px 50px #5E5E5E;
+}
+</style>
+</head>
+<body>
+<center>
+<div class="heading"><h1>Leave Management System</h1></div>
+<?php include 'navi.php';?>
+<div class="heading"><h2>Welcome !</h2>
+<p>A Small Leave Management System,this is created for satyanand dimri government polytechnic and fully created by mayank aggarwal and narendra painuly.</p>
+<footer>&copy; 2022, satyanand dimri government polytechnic.in</footer>
+</center>
+</div>
+</body>
+</html>
